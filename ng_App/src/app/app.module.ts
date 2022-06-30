25ca7c6a403c490b5784daf723fff63107ce6f1c
Safiullah Omar <omar.safiullah@gmail.com>
2022-06-30 21:49:58 +0430

    dashbaord

--- ng_App/src/app/app.module.ts
+++ ng_App/src/app/app.module.ts
@@ -9,13 +9,15 @@ import{ HttpClientModule  } from '@angular/common/http';
 import { RegisterComponent } from './register/register.component';
 import { UserService } from './user.service';
 import { UsrMgtComponent } from './usr-mgt/usr-mgt.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegisterComponent,
-    UsrMgtComponent
+    UsrMgtComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
